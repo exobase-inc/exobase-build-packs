704375f4ca3aa1295b0335f1caf0b982250250f2
Ray Epps <rayharryepps@gmail.com>
2022-03-13 20:14:26 -0600

    try file caching

--- packages/api/aws-lambda-typescript/index.ts
+++ packages/api/aws-lambda-typescript/index.ts
@@ -4,11 +4,9 @@ import * as aws from "@pulumi/aws"
 import fs from "fs-extra"
 import type { DeploymentContext } from "@exobase/client-js"
 import { AWSLambdaAPI } from "@exobase/pulumi-aws-lambda-api"
-import { ModuleFunction, getFunctionMap } from '@exobase/builds'
+import { getFunctionMap } from '@exobase/builds'
 import path from "path"
 import cmd from 'cmdish'
-import webpack from 'webpack'
-import TerserPlugin from 'terser-webpack-plugin'
 
 type Config = {
   timeout: number
@@ -33,9 +31,6 @@ const main = async ({
   const { platform, service, deployment } = context
   const config = deployment.config.stack as Config
 
-  console.log('x--> CONTEXT:')
-  console.log(JSON.stringify(context, null, 2))
-
   //
   //  SETUP PROVIDER
   //
@@ -53,14 +48,13 @@ const main = async ({
     ext: 'ts'
   })
 
-  console.log('x--> FUNCTIONS:')
-  console.log(functions)
-
   //
   //  EXECUTE BUILD
   //
-  await cmd('yarn && yarn build', { cwd: path.resolve(workingDir, 'source') })
-  // await executeBuild(functions, path.resolve(workingDir, 'source'))
+  await onlyOnce('build', async () => {
+    await cmd('yarn && yarn build', { cwd: path.resolve(workingDir, 'source') })
+    return true // cached value
+  })()
 
   //
   //  CREATE API/LAMBDA RESOURCES
@@ -100,63 +94,19 @@ const toNumber = (value: string | number): number => {
   return value as number
 }
 
-
-const executeBuild = async (functions: ModuleFunction[], sourceDir: string): Promise<void> => {
-  await cmd('rm -rf build', { cwd: sourceDir })
-  const execute = async (func: ModuleFunction) => {
-    console.log(`processing: ${func.module}/${func.function}.js`)
-    await compile(func, sourceDir)
-    console.log(`compiled: ${func.module}/${func.function}.js`)
-    await zip(func, sourceDir)
-    console.log(`zipped: ${func.module}/${func.function}.js -> ${func.module}/${func.function}.zip`)
-  }
-  await (Promise as any).allSettled([functions[0]].map(f => execute(f)))
-}
-
-const compile = async (func: ModuleFunction, sourceDir: string) => {
-  await new Promise<void>((res, rej) => {
-    webpack(
-      {
-        entry: [path.resolve(sourceDir, `src/modules/${func.module}/${func.function}.ts`)],
-        mode: (process.env.NODE_ENV as 'production' | 'development') ?? 'production',
-        target: 'node',
-        output: {
-          path: path.resolve(sourceDir, 'build', 'modules', func.module),
-          filename: `${func.function}.js`
-        },
-        resolve: {
-          extensions: ['.ts', '.js']
-        },
-        module: {
-          rules: [
-            {
-              test: /\.ts$/,
-              use: ['ts-loader']
-            }
-          ]
-        },
-        optimization: {
-          minimizer: [
-            new TerserPlugin({
-              extractComments: false
-            })
-          ]
-        }
-      },
-      (err, stats) => {
-        console.log('x--> WEBPACK COMPLETE:')
-        console.log(stats)
-        if (err || stats.hasErrors()) rej(err ?? { message: 'Webpack stats has error' })
-        else res()
-      }
-    )
-  })
-}
-
-const zip = async (func: ModuleFunction, sourceDir: string) => {
-  await cmd(`zip -q ${func.function}.zip ${func.function}.js`, {
-    cwd: path.resolve(sourceDir, 'build', 'modules', func.module)
-  })
+const onlyOnce = <T> (key: string, func: () => Promise<T>) => async (): Promise<T> => {
+  const cacheFilePath = path.resolve(process.cwd(), '.exo-operation-cache')
+  const cache = await (async () => {
+    const [notExists, existing] = await _.try(() => fs.readJSON(cacheFilePath))()
+    if (!notExists) return existing
+    await fs.writeJSON(cacheFilePath, {})
+    return {}
+  })()
+  if (cache[key]) return cache[key]
+  const [err, result] = await _.try(func)()
+  await fs.writeJSON(cacheFilePath, { ...cache, [key]: result })
+  if (err) throw err
+  else return result
 }
 
 export default main
